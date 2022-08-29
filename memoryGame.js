53914828e96a329e09cc7e7c531f70a87994febd
ofirshtrosberg <ofiri112@gmail.com>
2022-08-29 15:02:52 +0300

    29.8 15:02

--- memoryGame.js
+++ memoryGame.js
@@ -1,7 +1,7 @@
 $(() => {
-    
+    var audio = new Audio("http://music.ogg");
     // הכנסת שם ומספר קלפים 
-    
+
     //הוספה דינאמית של כרטיסים
 
     //מציאת זוג + אולי סאונד
@@ -12,6 +12,44 @@ $(() => {
         axis: 'y',
         trigger: 'click'
     });
+    var clickedCounter = 0;  // how many cards are clicked now 
+    $(".flipCard").click(function () {
+        console.log("click");
+        clickedCounter++;
+        $(this).addClass("clickedCard");
+        if (clickedCounter == 2) {
+            var card1 = document.getElementsByClassName("clickedCard")[0];
+            var card2 = document.getElementsByClassName("clickedCard")[1];
+
+            var img1 = card1.getElementsByClassName("backImg")[0].src;
+            var img2 = card2.getElementsByClassName("backImg")[0].src;
+
+            if (img1 == img2) {
+                // audio.oncanplaythrough = function(){
+                //     audio.play();
+                // }
+                $(".clickedCard").off(".flip"); // turns off the ability to flip
+                clickedCounter = 0;
+                $(".clickedCard").removeClass(".flipCard"); //turns off the ability to click this card 
+                $(".clickedCard").removeClass("clickedCard");
+            }
+            else{
+                setTimeout(function(){
+                    $(".clickedCard").flip(false);
+                    $(".clickedCard").removeClass("clickedCard");
+                    clickedCounter = 0;
+                }, 1900); 
+            } 
+        }
+    });
+
+
+
+
+
+
+
+
     // $(".flipCard").off(".flip");
 
 
