fb091aff444de886e70ccd78adb95df01bb70441
ofirshtrosberg <ofiri112@gmail.com>
2022-08-29 15:19:18 +0300

    15:19

--- memoryGame.js
+++ memoryGame.js
@@ -14,44 +14,36 @@ $(() => {
     });
     var clickedCounter = 0;  // how many cards are clicked now 
     $(".flipCard").click(function () {
-        console.log("click");
-        clickedCounter++;
-        $(this).addClass("clickedCard");
-        if (clickedCounter == 2) {
-            var card1 = document.getElementsByClassName("clickedCard")[0];
-            var card2 = document.getElementsByClassName("clickedCard")[1];
+        if(!$(this).hasClass("disabledClick")){ 
+            // console.log("click");
+            clickedCounter++;
+            $(this).addClass("clickedCard");
+            if (clickedCounter == 2) {
+                var card1 = document.getElementsByClassName("clickedCard")[0];
+                var card2 = document.getElementsByClassName("clickedCard")[1];
 
-            var img1 = card1.getElementsByClassName("backImg")[0].src;
-            var img2 = card2.getElementsByClassName("backImg")[0].src;
+                var img1 = card1.getElementsByClassName("backImg")[0].src;
+                var img2 = card2.getElementsByClassName("backImg")[0].src;
 
-            if (img1 == img2) {
-                // audio.oncanplaythrough = function(){
-                //     audio.play();
-                // }
-                $(".clickedCard").off(".flip"); // turns off the ability to flip
-                clickedCounter = 0;
-                $(".clickedCard").removeClass(".flipCard"); //turns off the ability to click this card 
-                $(".clickedCard").removeClass("clickedCard");
-            }
-            else{
-                setTimeout(function(){
-                    $(".clickedCard").flip(false);
-                    $(".clickedCard").removeClass("clickedCard");
+                if (img1 == img2) {
+                    
+                    // audio.play();
+                    
+                    $(".clickedCard").off(".flip"); // turns off the ability to flip
                     clickedCounter = 0;
-                }, 1900); 
-            } 
-        }
+                    $(".clickedCard").addClass('disabledClick');
+                    // $(".clickedCard").removeClass(".flipCard"); //turns off the ability to click this card 
+                    // $(".clickedCard").removeClass("clickedCard");
+                }
+                else{
+                    setTimeout(function(){
+                        $(".clickedCard").flip(false);
+                        $(".clickedCard").removeClass("clickedCard");
+                        clickedCounter = 0;
+                    }, 1900); 
+                } 
+            }
+    }
     });
-
-
-
-
-
-
-
-
-    // $(".flipCard").off(".flip");
-
-
     //סיום המשחק 
 });
